c86888229f0920362f2535f825bb7a09a896ce0b
Daniela <dannytorres0211@gmail.com>
2022-09-29 20:50:21 -0500

    feat: add image to logo and width and height properties

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -5,14 +5,24 @@ type Props = {
   logo: string // wp.png
   phone: string // 3505555
   message: string // Estás comunicándote con Blush-Bar, por favor ingresa tu duda.
+  width: number // 80px
+  height: number // 80px
 }
 
-const WhatsappButton = ({ logo, phone, message }: Props) => {
+const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
+  const formattedMessage = message.replace(/ /g, '%20')
+
   return (
     <>
-      <p>{logo}</p>
-      <p>{phone}</p>
-      <p>{message}</p>
+      <div className="fixed bottom-1 right-2 flex felxColumn">
+        <a
+          href={`https://wa.me/${phone}?text=${formattedMessage}`}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={logo} width={width} height={height} alt="Logo WP" />
+        </a>
+      </div>
     </>
   )
 }
@@ -21,12 +31,16 @@ WhatsappButton.propTypes = {
   logo: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
   message: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
 }
 
 WhatsappButton.defaultProps = {
   logo: 'mi-logo.png',
   phone: '3004507575',
   message: 'Estás comunicándote con Blush-Bar, por favor ingresa tu duda.',
+  width: 70,
+  height: 70,
 }
 
 WhatsappButton.schema = {
@@ -52,6 +66,14 @@ WhatsappButton.schema = {
       widget: {
         'ui:widget': 'textarea',
       },
+    },
+    width: {
+      title: 'Width',
+      type: 'number',
+    },
+    height: {
+      title: 'Height',
+      type: 'number',
     },
   },
 }
